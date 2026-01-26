a951908aac53b2d6f32c930005ec52d2fab73227
Achir Jain <achirjain11@gmail.com>
2026-01-26 17:58:49 +0530

    add ChatMemory by adding MessageChatMemoryAdvisor as a default advisor during ChatMemory bean creation using OllamaChatModel

--- src/main/java/com/springAiTemplate/Chatbot/Configurations/ChatClientConfig.java
+++ src/main/java/com/springAiTemplate/Chatbot/Configurations/ChatClientConfig.java
@@ -1,6 +1,10 @@
 package com.springAiTemplate.Chatbot.Configurations;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +12,11 @@ import org.springframework.context.annotation.Configuration;
 public class ChatClientConfig {
 
     @Bean
-    ChatClient ollamaChatClient(ChatClient.Builder builder){
-        return builder.build();
+    ChatClient getOllamaChatClient(OllamaChatModel chatModel){
+        ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+        ChatClient chatClient = ChatClient.builder(chatModel)
+            .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+            .build();
+        return chatClient;
     }
 }

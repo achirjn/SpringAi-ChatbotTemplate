ba5aa4fa933fdbddad0cbd6dd4ae7dea065cc006
Achir Jain <achirjain11@gmail.com>
2026-02-02 23:58:28 +0530

    moving towards manual RAG implementation

--- src/main/java/com/springAiTemplate/Chatbot/Configurations/VectorStoreConfig.java
+++ src/main/java/com/springAiTemplate/Chatbot/Configurations/VectorStoreConfig.java
@@ -1,46 +1,54 @@
 package com.springAiTemplate.Chatbot.Configurations;
 
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.ollama.OllamaEmbeddingModel;
+import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaEmbeddingOptions;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
-// import redis.clients.jedis.JedisPooled;
+import redis.clients.jedis.JedisPooled;
 
 @Configuration
 public class VectorStoreConfig {
 
-    //  @Bean
-    // public OllamaApi ollamaApi(RestClient.Builder restClientBuilder) {
-    //     return OllamaApi.builder()
-    //             .baseUrl("http://localhost:11434")
-    //             .restClientBuilder(restClientBuilder)
-    //             .build();
-    // }
+    @Bean
+    public OllamaApi ollamaApi(RestClient.Builder restClientBuilder) {
+        return OllamaApi.builder()
+                .baseUrl("http://localhost:11434")
+                .restClientBuilder(restClientBuilder)
+                .build();
+    }
 
-    // @Bean
-    // public EmbeddingModel embeddingModel(OllamaApi ollamaApi) {
+    @Bean
+    public EmbeddingModel embeddingModel(OllamaApi ollamaApi) {
 
-    //     return OllamaEmbeddingModel.builder()
-    //             .ollamaApi(ollamaApi)
-    //             .defaultOptions(
-    //                     OllamaEmbeddingOptions.builder()
-    //                             .model("nomic-embed-text")
-    //                             .build()
-    //             )
-    //             .build();
-    // }
+        return OllamaEmbeddingModel.builder()
+                .ollamaApi(ollamaApi)
+                .defaultOptions(
+                        OllamaEmbeddingOptions.builder()
+                                .model("nomic-embed-text")
+                                .build()
+                )
+                .build();
+    }
 
-    //  @Bean
-    // public JedisPooled jedisPooled() {
-    //     return new JedisPooled("localhost", 6379);
-    // }
+     @Bean
+    public JedisPooled jedisPooled() {
+        return new JedisPooled("localhost", 6379);
+    }
 
-    // @Bean
-    // public VectorStore vectorStore(
-    //         JedisPooled jedisPooled,
-    //         EmbeddingModel embeddingModel) {
+    @Bean
+    public VectorStore vectorStore(
+            JedisPooled jedisPooled,
+            EmbeddingModel embeddingModel) {
 
-    //     return RedisVectorStore.builder(jedisPooled, embeddingModel)
-    //             .indexName("rag-index")
-    //             .initializeSchema(true)
-    //             .build();
-    // }
+        return RedisVectorStore.builder(jedisPooled, embeddingModel)
+                .indexName("rag-index")
+                .initializeSchema(true)
+                .build();
+    }
 }
--- src/main/java/com/springAiTemplate/Chatbot/Controllers/HomeController.java
+++ src/main/java/com/springAiTemplate/Chatbot/Controllers/HomeController.java
@@ -1,9 +1,20 @@
 package com.springAiTemplate.Chatbot.Controllers;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.springAiTemplate.Chatbot.Helper.DocumentIngestor;
 
 
 
@@ -11,11 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    // private final DocumentIngestor ingestor;
+    private final DocumentIngestor ingestor;
 
-    // public HomeController(DocumentIngestor ingestor){
-    //     this.ingestor = ingestor;
-    // }
+    public HomeController(DocumentIngestor ingestor){
+        this.ingestor = ingestor;
+    }
 
     @GetMapping("/health")
     public ResponseEntity<?> getHealth() {
@@ -27,18 +38,27 @@ public class HomeController {
     //     ingestor.ingest(List.of(policyText));
     // }
     
-    // @PostMapping("ingest-file")
-    // public void uploadPolicyDoc(@RequestParam("file") MultipartFile file) {
-    //     try{
-    //         Path tempFile = Files.createTempFile("upload-", ".txt");
-    //         file.transferTo(tempFile.toFile());
-    //         Resource resource = new FileSystemResource(tempFile);
-    //         ingestor.ingestResources(List.of(resource));
-    //     }
-    //     catch(Exception e){
-    //         e.printStackTrace();
-    //     }
-    // }
-    
+    @PostMapping("ingest-file")
+    public ResponseEntity<String> uploadPolicyDoc(@RequestParam("file") MultipartFile file) {
+        Path tempFile = null;
+
+        try {
+            tempFile = Files.createTempFile("upload-", file.getOriginalFilename());
+            file.transferTo(tempFile.toFile());
+
+            Resource resource = new FileSystemResource(tempFile);
+            ingestor.ingestResources(List.of(resource));
+
+            return ResponseEntity.ok("File ingested");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        } finally {
+            if (tempFile != null) {
+                try { Files.deleteIfExists(tempFile); } catch (Exception ignored) {}
+            }
+        }
+    }
+
     
 }

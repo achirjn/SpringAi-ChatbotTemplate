97bbbe5541aa308cca9944bfafb6229aede5b61c
Achir Jain <achirjain11@gmail.com>
2026-02-03 00:04:11 +0530

    change temperature to 0.0 and maxTokens to 200

--- src/main/java/com/springAiTemplate/Chatbot/Configurations/ChatClientConfig.java
+++ src/main/java/com/springAiTemplate/Chatbot/Configurations/ChatClientConfig.java
@@ -1,8 +1,6 @@
 package com.springAiTemplate.Chatbot.Configurations;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +11,12 @@ public class ChatClientConfig {
 
     @Bean
     ChatClient getOllamaChatClient(OllamaChatModel chatModel){
-        MessageWindowChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+        // MessageWindowChatMemory chatMemory = MessageWindowChatMemory.builder().build();
         ChatClient chatClient = ChatClient.builder(chatModel)
-            .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
-            .defaultSystem("your response should be very brief. i have put up response limit to 200 tokens so complete your answer in this limit.")
+            // .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
             .defaultOptions(ChatOptions.builder()
                         .maxTokens(200)
-                        .temperature(0.2)
+                        .temperature(0.0)
                         .build())
             .build();
         return chatClient;
